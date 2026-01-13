c6086734fcb33fd7e9d78e9d4ab9e7d14f0aa1e4
Chris Gradwohl <15326707+cgradwohl@users.noreply.github.com>
2026-01-13 11:33:15 -0800

    feat:

--- services/beetle/src/go.mod
+++ services/beetle/src/go.mod
@@ -1,5 +1,3 @@
 module github.com/cgradwohl/beetle
 
 go 1.20
-
-require sigs.k8s.io/yaml v1.4.0
--- services/beetle/src/main.go
+++ services/beetle/src/main.go
@@ -1,50 +1,10 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
-	"time"
-
-	"sigs.k8s.io/yaml"
 )
 
 func main() {
-	// Paths where the ConfigMap files will be mounted
-	// paths := map[string]string{
-	// 	"global":  "/etc/config/global-config.yaml",
-	// 	"service": "/etc/config/service-config.yaml",
-	// }
-	//
-	// out := make(map[string]interface{})
-	//
-	// for key, path := range paths {
-	// 	data, err := ioutil.ReadFile(path)
-	// 	if err != nil {
-	// 		log.Printf("WARN: could not read %s (%s): %v", key, path, err)
-	// 		continue
-	// 	}
-	//
-	// 	var parsed interface{}
-	// 	if err := yaml.Unmarshal(data, &parsed); err != nil {
-	// 		log.Fatalf("ERROR: failed to parse %s: %v", path, err)
-	// 	}
-	//
-	// 	out[key] = parsed
-	// }
-	//
-	// pretty, err := json.MarshalIndent(out, "", "  ")
-	// if err != nil {
-	// 	log.Fatalf("ERROR: failed to marshall JSON: %v", err)
-	// }
-	//
-	// fmt.Println(string(pretty))
 	fmt.Println("hello creature...")
-	//
-	// // Keep container alive so you can check logs
-	// for {
-	// 	time.Sleep(10 * time.Minute)
-	// }
 }
 
