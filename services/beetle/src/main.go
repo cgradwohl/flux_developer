9c3acd7a68e22f31dba8e14a08e33754e4721e6e
Chris Gradwohl <15326707+cgradwohl@users.noreply.github.com>
2026-01-13 14:06:19 -0800

    feat:

--- services/beetle/src/main.go
+++ services/beetle/src/main.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("hello creature...")
+	fmt.Println("beetle starting on :8080")
+
+	http.HandleFunc("/healthz", func(w http.ResponseWriter, _ *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("ok"))
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
 
